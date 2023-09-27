f572f0360912d7f7b025e97310c2d3dd7c610356
jeetpal-lab <145384781+jeetpal-lab@users.noreply.github.com>
2023-09-27 18:56:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,16 @@
 import streamlit as st
-import validators
 import requests
 from bs4 import BeautifulSoup
-from gensim.summarization import summarize
 
 # Streamlit app title and description
-st.title("Web Content Summarizer")
-st.write("Enter a URL to generate a summary of its content.")
+st.title("Web Content Extractor")
+st.write("Enter a URL and click the button to extract its content.")
 
 # User input for the URL
 url = st.text_input("Enter a URL:", "")
 
-# Function to validate a URL
-def is_valid_url(url):
-    if validators.url(url):
-        return True
-    else:
-        return False
-
-# Function to extract and summarize web content
-def summarize_web_content(url):
+# Function to extract and display web content
+def extract_content(url):
     try:
         # Send a GET request to the URL
         response = requests.get(url)
@@ -31,25 +22,20 @@ def summarize_web_content(url):
         # Extract the text content from the page
         page_content = soup.get_text()
 
-        # Generate a summary of the content
-        summary = summarize(page_content)
-
-        # Display the summary
-        st.subheader("Generated Summary:")
-        st.write(summary)
+        # Display the extracted content
+        st.subheader("Extracted Content:")
+        st.write(page_content)
 
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
 
-# Button to trigger content summarization and URL validation
-if st.button("Summarize Content"):
+# Button to trigger content extraction
+if st.button("Extract Content"):
     if url:
-        if is_valid_url(url):
-            summarize_web_content(url)
-        else:
-            st.error("Invalid URL. Please enter a valid URL.")
+        extract_content(url)
     else:
-        st.warning("Please enter a URL.")
+        st.warning("Please enter a valid URL.")
 
 # Display a footer
-st.write("Jeetendra")
+st.write("Created by Jeetendra")
+
